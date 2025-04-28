0ecd593a9c28b9beb7a57f380d7760c4721c64be
Phanupong-M <panupong38240@gmail.com>
2025-04-28 15:22:53 +0700

    fix(cros): update CORS config

--- app.mjs
+++ app.mjs
@@ -7,25 +7,14 @@ import authRouter from "./routes/auth.mjs";
 const app = express();
 const port = process.env.PORT || 4001;
 
-const allowedOrigins = [
-  process.env.CLIENT_URL,
- 'http://localhost:5173',
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    console.log('Request Origin:', origin);
-    console.log('Allowed CLIENT_URL:', process.env.CLIENT_URL);
-    console.log('Is Origin Allowed?', !origin || allowedOrigins.includes(origin));
-  
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(cors());
 app.use(express.json());
